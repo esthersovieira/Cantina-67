ca69f2aa1e7467500188fa773021abdb6e570f0a
Esther Sophia Vieira <denisevieira280987@gmail.com>
2026-05-06 21:43:03 -0300

    site cantina

--- produtos/views.py
+++ produtos/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from .models import Produto
+
+def home(request):
+    # Filtra apenas o que está disponível e embaralha a ordem
+    salgados = Produto.objects.filter(disponivel=True).order_by('?')[:6]
+    return render(request, 'produtos/home.html', {'salgados': salgados})
+
+def cardapio(request):
+    # Busca todos os produtos para exibir ao público
+    produtos = Produto.objects.all().order_by('nome')
+    return render(request, 'produtos/cardapio.html', {'produtos': produtos})
